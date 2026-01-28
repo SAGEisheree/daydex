3751ce9d130e3b3f6dac3b524d1396819c03480f
Abhinav <abhinavakarthikeya01@gmail.com>
2026-01-28 20:49:57 +0530

    Added note taking feature & Changed the popup menu

--- src/things/Day.jsx
+++ src/things/Day.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 
+/////////////////      logic part ///////////////////////////////////////////////////////////
+// To save the color buttons and open popup/////////////////////////////////////////////////////////////////
+
 const Day = ({ name, day }) => {
 
     const storageKey = `mood-${name}-${day}`;
@@ -12,9 +15,7 @@ const Day = ({ name, day }) => {
 
     const pickColor = (colorClass) => {
         setSelectedColor(colorClass);
-        localStorage.setItem(storageKey, colorClass);
-        setIsOpen(false);
-    }
+        localStorage.setItem(storageKey, colorClass);}
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -24,7 +25,19 @@ const Day = ({ name, day }) => {
         }
     }, [isOpen]);
 
+    // To save the notes from noteInput //////////////////////////////////////////////////////////////////
+    const noteTextKey = `noteText-${name}-${day}`;
 
+    const [noteText, setNoteText] = useState(() => {
+        return localStorage.getItem(noteTextKey) || '';
+    });
+    const handleSave = () => {
+        localStorage.setItem(noteTextKey, noteText);
+    };
+
+    // Main visual content//////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
     return (
         <>
             <button
@@ -37,31 +50,49 @@ const Day = ({ name, day }) => {
             >
                 {day}
             </button>
+            {/* /////////////////////////////   popUP  /////////////////////////////////////////////////////// */}
 
             {isOpen && <dialog
                 ref={modalRef}
                 onClose={() => setIsOpen(false)}
                 id={`modal-${name}-${day}`} className="modal">
-                <div className="modal-box h-96 pt-12 flex flex-col justify-between">
-                    <div className="h-80 w-64  border-t-4 border-gray-600  shadow-md fixed ">
-                        <h2 className="text-center text-2xl mt-2 ">Select a mood</h2>
-                        <div className="flex flex-col m-4 justify-between h-56 ">
-                            <button onClick={() => pickColor('bg-emerald-500')} className='btn btn-ghost  bg-emerald-500 '>Super Good</button>
-                            <button onClick={() => pickColor('bg-lime-500')} className='btn btn-ghost  bg-lime-500 '>Good</button>
-                            <button onClick={() => pickColor('bg-orange-500')} className='btn btn-ghost  bg-orange-500 '>Bad</button>
-                            <button onClick={() => pickColor('bg-rose-500')} className='btn btn-ghost bg-rose-500  '>Very Bad</button>
-                            <button onClick={() => pickColor('bg-base-200')} className='btn btn-ghost  bg-base-300 '>None</button>
+                <div className="modal-box h-auto pt-4 pb-0 flex md:w-xl flex-col justify-between">
+
+                    <div className="flex max-sm:flex-wrap  flex-row m-0 justify-between h-fit">
+                        <button onClick={() => pickColor('bg-emerald-500')} className='btn btn-ghost  bg-emerald-500 '>Super Good</button>
+                        <button onClick={() => pickColor('bg-lime-500')} className='btn btn-ghost  bg-lime-500 '>Good</button>
+                        <button onClick={() => pickColor('bg-orange-500')} className='btn btn-ghost  bg-orange-500 '>Bad</button>
+                        <button onClick={() => pickColor('bg-rose-500')} className='btn btn-ghost bg-rose-500  '>Very Bad</button>
+                        <button onClick={() => pickColor('bg-base-200')} className='btn btn-ghost bg-base-200  '>No Color</button>
+                    </div>
+
+
+                    <div>
+                        <div className="flex flex-col max-sm:m-1 m-4 mb-0">
+                            <textarea
+                                value={noteText}
+                                onChange={(e) => setNoteText(e.target.value)}
+                                placeholder="How was ur day?"
+                                className="border-2 border-gray-600 bg-base-100 max-sm:h-32 h-24 w-fill">
+                            </textarea>
+
+                            <button
+                                onClick={handleSave}
+                                className="btn  bg-base-200 btn-small">
+                                Add note
+                            </button>
+                            <button
+                                onClick={() => setIsOpen(false)}
+                                className="bg-red-400 btn w-20 m-2 ml-80 max-sm:ml-56 btn-primary z-10">Close</button>
+
                         </div>
                     </div>
-                    
-                    <div  method="dialog">
-                        <button 
-                        onClick={() => setIsOpen(false)} 
-                        className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                    </div>
+
+
                 </div>
             </dialog>}
         </>
     );
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
 };
 export default Day;
