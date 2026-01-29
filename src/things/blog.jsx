79349e13287d25c1777fe5c7362188d565e5daf4
Abhinav <abhinavakarthikeya01@gmail.com>
2026-01-29 22:32:39 +0530

    added development logs UI to blog

--- src/things/blog.jsx
+++ src/things/blog.jsx
@@ -1,12 +1,29 @@
 
 
 const BlogPage = () => {
-  return (
-    <div>
-     <h1>Blogs </h1>
 
+
+  return (
+    <div className="bg-base-100 m-80 mt-10  max-md:m-8 h-screen ">
+      <h1 className="text-xl md:text-3xl font-bold">Development Logs</h1>
+      <p> Documeting the journey in making this project</p>
+      <div className="  h-screen">
+       
+       <div className="p-2 mt-10 border-l-4 border-base-300">
+          <p className="text-xl mb-4">29/1/2026</p>
+          <div className="bg-base-200 border border-base-300 p-4">
+            Added routes for better code readability and also added the  blog you are reading.
+          </div>
+        </div>
+        <div className="p-2 mt-10 border-l-4 border-base-300">
+          <p className="text-xl mb-4">28/1/2026</p>
+          <div className="bg-base-200 border border-base-300 p-4">
+            Changed the UI of popup and also added the note taking feature.
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default BlogPage;
--- src/things/realpage.jsx
+++ src/things/realpage.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
+import { Link } from "react-router"
 import { useState } from 'react'
 import Month from './month.jsx'
 import BlogPage from './blog.jsx'
 
 const RealPage = () => {
-  
-     const [blogOpen, setBlogOpened ] = useState(false)
+
   return (
-    <div data-theme="retro"  className="font-['Kode Mono'] width-100%">
+    <div data-theme="retro" className="font-['Kode Mono'] width-100%">
       <div className="h-14 bg-base-300/35 border-b-[1px] border-gray-400 z-10 sticky backdrop-blur-[7px] top-0 w-full flex items-center justify-between">
         <h1 className=" ml-4 text-3xl max-sm:text-xl font-bold text-center">Mood Metric</h1>
-        <button 
-        onClick={()=>setBlogOpened(true)
-        }
-        className="btn btn-outline mr-10 w-24">Blog</button>
+        <Link to="/blog">
+          <button
+            className="btn btn-outline mr-10 w-24">Blog
+          </button>
+        </Link>
       </div>
 
-    
-     {blogOpen && <link to={`/blog`}/>}
 
-      { blogOpen!=true &&  <div className=" flex md:flex-row  flex-col">
+
+      <div className=" flex md:flex-row  flex-col">
         <div className=" w-screen  ">
           <div className="h-80 md:w-64 border-t-4 border-gray-600 m-4 shadow-md ">
             <h2 className="text-center text-2xl mt-2 ">Moods</h2>
@@ -28,7 +28,7 @@ const RealPage = () => {
               <button className='btn btn-ghost  bg-emerald-500 '>Super Good</button>
               <button className='btn btn-ghost  bg-lime-500 '>Good</button>
               <button className='btn btn-ghost  bg-orange-500 '>Bad</button>
-              <button className='btn btn-ghost  bg-rose-500'>Very Bad</button>             
+              <button className='btn btn-ghost  bg-rose-500'>Very Bad</button>
             </div>
           </div>
         </div>
@@ -36,10 +36,10 @@ const RealPage = () => {
           <Month />
         </div>
 
-      </div>}
+      </div>
 
     </div>
-    
+
   )
 }
 
