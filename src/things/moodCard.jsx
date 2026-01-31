1ba2e7bd126c66ba9eaf50b965db46836fa0b44e
Abhinav <abhinavakarthikeya01@gmail.com>
2026-01-31 23:36:49 +0530

    Wrote color changing logic of moods..logic applies only in the card

--- src/things/moodCard.jsx
+++ src/things/moodCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-import { Pencil, Check } from 'lucide-react';
+import { Pencil} from 'lucide-react';
 
 const MoodCard = () => {
 
@@ -9,10 +9,10 @@ const MoodCard = () => {
     { id: 1, name: "SuperGood", color: "bg-emerald-500" },
     { id: 2, name: "Good", color: "bg-lime-500" },
     { id: 3, name: "not bad", color: "bg-orange-500" },
-    { id: 3, name: "bad", color: "bg-red-500" },
+    { id: 4, name: "bad", color: "bg-red-500" },
   ]);
 
-  const colors = ["bg-emerald-500", "bg-lime-500", "bg-yellow-500", "bg-orange-500", "bg-red-500", "bg-pink-500", "bg-rose-500", "bg-fuchsia-500", "bg-purple-500", "bg-violet-500", "bg-indigo-500", "bg-blue-500", "bg-teal-500", "bg-green-500"]
+  const colors = ["bg-emerald-500", "bg-lime-500", "bg-yellow-500", "bg-orange-500", "bg-red-500", "bg-rose-500", "bg-fuchsia-500", "bg-purple-500", "bg-violet-500",  "bg-blue-500", "bg-teal-500", "bg-green-500"]
 
   const updateItem = (id, updates) => {
     const newItems = items.map((item) => {
@@ -42,25 +42,33 @@ const MoodCard = () => {
         <div className="flex flex-col m-4  h-fit ">
 
           {items.map((item) => (
-            <div className={`${item.color} flex pt-3 pl-4 mb-4 h-12 flex-row rounded-md justify-between`}>
+            <div className={`${item.color} flex pt-3 pl-4 mb-4 h-12 flex-wrap rounded-md justify-between`}>
               <div className={`text-center font-semibold `}>
                 {item.name}
               </div>
-              <button className="pr-3 pb-3">
+              <button
+                onClick={() => setEditingId(editingId === item.id ? null : item.id)}
+                className="pr-3 pb-3">
                 <Pencil size={18} />
               </button>
 
 
+              {editingId === item.id && (
+                <div className="flex flex-wrap z-10 bg-base-300 border-2 p-3 rounded-md border-gray-500 shadow-md">
+                  {colors.map((e) => 
+                  <button 
+                  key={e}
+                  onClick={() => {updateItem(item.id, { color: e})
+                    }
+                  }
+                  className={`${e} h-8 w-8 m-1 border-2 border-gray-600 rounded-md`}>
+                  </button>)}
+                </div>
+              )}
+
+              
             </div>
-
-
-          )
-          )}
-
-
-
-
-
+          ))}
         </div>
       </div>
     </div>
