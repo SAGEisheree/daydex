41363125e749d410978349d600993924d25974b5
Abhinav <abhinavakarthikeya01@gmail.com>
2026-02-24 18:27:44 +0530

    Revert "made ready for deploying with render"
    This reverts commit b8b9ec4daa59630c13b30cb5483a1bf5602ad725.

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,12 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import path from 'path'
 
-import storageRoutes from './src/routes/storageRoutes.js'
+import storageRoutes from './routes/storageRoutes.js'
 
 const app = express();
-const __dirname = path.resolve();
-
-
-
 // middlewware
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
+app.use(cors());
 app.use(express.json());
 
 // connecting mongo DB to server
@@ -37,17 +26,6 @@ connectDB();
 
 //routes
 app.use('/api/storage', storageRoutes);
-
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*any", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-
 app.get('/', (req,res) =>res.send('API is ruunning'));
 
 //server status
--- frontend/src/hooks/useCloudStorage.js
+++ frontend/src/hooks/useCloudStorage.js
@@ -1,34 +1,39 @@
-import { useState, useEffect } from 'react';
-import api from '../api/axios';
+import {useState, useEffect} from 'react';
+import axios from 'axios'
 
 const useCloudStorage = (key, initialValue) => {
-    const [state, setState] = useState(initialValue);
 
-    useEffect(() => {
+
+    const [state, setState] = useState(initialValue);
+    const API_URL = `http://localhost:5000/api/storage`;
+
+    // pulling the data from DB after components load
+
+    useEffect(() =>{
         const fetchData = async () => {
             try {
-                const res = await api.get(`/storage/${key}`);
-                if (res.data !== null) {
-                    setState(res.data);
+                const res=await axios.get(`${API_URL}/${key}`);
+                if(res.data !== null) {
+                    setState(res.data)
                 }
             } catch (err) {
-                console.error("Error loading from cloud:", err);
+                console.error("Error lodaing from clous:", err);
             }
-        };
+        }   
         fetchData();
     }, [key]);
+    
+  // syncing localstorage to cloud
+  const setValue = async (value)=>{
+    try {
+        setState(value);
+        await axios.post(API_URL, { key, value });
+    } catch (err) {
+        console.error("error saving to cloud", err);
+    }
+  };
+  return [state, setValue];
 
-    // Syncing to cloud
-    const setValue = async (value) => {
-        try {
-            setState(value);
-            await api.post('/storage', { key, value });
-        } catch (err) {
-            console.error("Error saving to cloud:", err);
-        }
-    };
-
-    return [state, setValue];
-};
+}
 
 export default useCloudStorage;
