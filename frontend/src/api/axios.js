5b297a32d17b4fffe150cb9325025bc9ef5006be
Abhinav <abhinavakarthikeya01@gmail.com>
2026-02-24 18:25:11 +0530

    Revert "initialised backend"
    This reverts commit 88c63b8860c27c62f66680d98fc124479eeb15d7.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" 
-  ? "http://localhost:5000/api" 
-  : "https://daydex.onrender.com/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://daydex.onrender.com/",
 });
 
 export default api;
